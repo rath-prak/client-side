0c82f1ce58a2a7c6d996010fdc454f4b113bb5bc
rath-prak <rathanap@gmail.com>
2016-04-04 11:48:44 +1200

    Updates

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var xhr = require('xhr')
 var example = require('./views/example.hbs')
 
-xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, data) {
+// xhr.get('https://api.wheretheiss.at/v1/satellites', function(err, dataFromApi) {
+//   if (err) console.log(err) // do something
+//   	var convertJsonData = JSON.parse(dataFromApi.body);
+
+//   	var nameFromJson = convertJsonData[0].name;
+//   	console.log(convertJsonData[0].name);
+
+//   document.body.innerHTML = example({ name: nameFromJson });
+// })
+
+
+
+xhr.get('https://api.wheretheiss.at/v1/satellites/25544', function(err, dataFromApi) {
   if (err) console.log(err) // do something
+  	var convertJsonData = JSON.parse(dataFromApi.body);
 
-  console.log(data.body)
-  document.body.innerHTML = example({ name: "Space" });
+
+  	var name = convertJsonData.name;
+  	console.log(convertJsonData.name);
+
+  	var nameFromJson = convertJsonData.latitude;
+  	console.log(convertJsonData.latitude);
+
+  document.body.innerHTML = example({ name: name, location : nameFromJson });
+  // document.body.innerHTML = example({ location : nameFromJson });
 })
-
